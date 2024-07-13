c4f5bdcb3232cc9e28c02c9e2381a2c18dd820a2
Harshad Prajapati <bholo0090@gmail.com>
2024-07-13 11:39:20 +0530

    new branch

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,7 @@ function Home() {
   return (
     <div className='h-screen bg-red-300 flex text-8xl justify-center items-center text-black/50  '>
       This is Home
-
-      <div className=' bg-black h-screen'></div>
-      
+  <h1> hello this is chekng </h1>
     </div>
   );
 }
