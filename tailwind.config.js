730f0ea0946abf46431af270c5aae74090752767
Mohit Kumar <mohitkr7982@gmail.com>
2024-07-18 10:25:37 +0530

    added prporty listing, tenats page,and aland lord part

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none',  /* IE and Edge */
+          'scrollbar-width': 'none',     /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',               /* Chrome, Safari, and Opera */
+        },
+      });
+    },
+  ],
 }
 
